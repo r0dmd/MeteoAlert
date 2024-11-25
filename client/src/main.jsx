4a538a5f30e3631ccd376ba2ec3f7835cd5e4832
Rodrigo Masid <remd884@mailfence.com>
2024-11-25 13:31:29 +0100

    useDocumentTitle

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/tailwind.css';
+
+import { AuthProvider } from './contexts/authContext.js';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx';
+
+// Estilos
+import './styles/tailwind.css';
 
 // ------------------------------------------
 // Punto de entrada principal de la aplicación. Se encarga de montar el árbol de componentes de React en el DOM. Incluye configuraciones globales como el modo estricto de React y enlaza el componente principal `App` al elemento raíz del HTML
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
