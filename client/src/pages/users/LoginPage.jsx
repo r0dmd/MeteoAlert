f73b18daa6a84af54e96e6fa1632e26cb04400c8
Rodrigo Masid <remd884@mailfence.com>
2024-11-27 22:13:38 +0100

    loginpage

--- client/src/pages/users/LoginPage.jsx
+++ client/src/pages/users/LoginPage.jsx
@@ -3,26 +3,61 @@ import { loginUser } from '../../api/index.js';
 import { AuthContext } from '../../contexts/index.js';
 
 import { useDocumentTitle } from '../../hooks/index.js';
+import { useGoHome } from '../../hooks/index.js';
 import toast from 'react-hot-toast';
 
 // ------------------------------------------
 const LoginPage = () => {
+  // Titulo de pestaña
   useDocumentTitle('Iniciar sesión');
 
-  const { authLoginState } = useContext(AuthContext);
+  // Importamos los datos del usuario y la función que almacena el token.
+  const { authUser, authLoginState } = useContext(AuthContext);
+
+  // Importamos el hook personalizado de Navigate
+  const goHome = useGoHome();
+
+  // Declaramos una variable en el State para definir el valor de cada input.
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  // Variable que indica cuando termina el fetch.
+  const [loading, setLoading] = useState(false);
+
+  // Función que maneje el envío del formulario.
   const handleLogin = async (e) => {
-    e.preventDefault();
     try {
+      // Prevenimos el comportamiento por defecto del formulario.
+      e.preventDefault();
+
+      // Indicamos que va a dar comienzo el fetch.
+      setLoading(true);
+
+      // Obtenemos una respuesta.
       const { token, user } = await loginUser({ email, password });
-      authLoginState(token); // Guardar el token en el contexto.
+      authLoginState(token);
       console.log('Usuario autenticado:', user);
+      toast.success('Bienvenido');
+      goHome();
+
+      // Redirigimos a la página principal.
+      toast.success('Bienvenido');
+      goHome();
     } catch (err) {
-      toast.error(err.message);
+      toast.error(err.message, {
+        id: 'login',
+      });
+    } finally {
+      // Indicamos que ha finalizado el fetch.
+      setLoading(false);
     }
   };
+
+  // Si estamos logeados restringimos el acceso redirigiendo a la página principal.
+  // En este caso utilizaremos el componente Navigate (en lugar de la función).
+  if (authUser) {
+    return goHome();
+  }
 
   return (
     <form onSubmit={handleLogin}>
@@ -40,7 +75,7 @@ const LoginPage = () => {
         placeholder='Contraseña'
         required
       />
-      <button type='submit'>Iniciar Sesión</button>
+      <button disabled={loading}>Iniciar Sesión</button>
     </form>
   );
 };
