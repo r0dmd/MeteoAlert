3b4461188dab2ad268543a0c9a15e52fccf9c625
Rodrigo Masid <remd884@mailfence.com>
2024-11-28 13:44:47 +0100

    fuentes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,7 +4,24 @@ import { useDocumentTitle } from '../hooks/index.js';
 const HomePage = () => {
   useDocumentTitle('Inicio');
 
-  return <p>test</p>;
+  return (
+    <>
+      <p className='font-poppins'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam soluta
+        suscipit saepe harum hic quam cupiditate corrupti dolorem officiis,
+        rerum minima ratione voluptatum illo aspernatur nemo excepturi
+        reprehenderit? Quos, nemo.
+      </p>
+      <h1 className='font-poppins text-2xl'>Texto con Poppins</h1>
+      <span className='font-montserrat'>Datos con Montserrat</span>
+      <p className='font-normal'>
+        Este texto usa el peso 400 (normal) de Inter.
+      </p>
+      <p className='font-medium'>
+        Este texto usa el peso 500 (medio) de Inter.
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
